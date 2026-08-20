6df6c097f7f71c25c0d89c433cd5cb5ed0c90dd5
sandeep <sandeep@gmail.com>
2026-08-20 10:01:35 +0530

    fxied isue

--- database/migrations/2026_08_11_000003_create_wallets_table.php
+++ database/migrations/2026_08_11_000003_create_wallets_table.php
@@ -7,6 +7,9 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
+        if (Schema::hasTable('wallets')) {
+        return;
+    }
         Schema::create('wallets', function (Blueprint $table) {
             $table->id();
             $table->foreignId('vendor_id')->constrained()->cascadeOnDelete();
