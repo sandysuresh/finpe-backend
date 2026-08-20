de50ababec8a8b50dd01bab31c51721524c80d08
sandeep <sandeep@gmail.com>
2026-08-20 10:02:27 +0530

    fxied isue

--- database/migrations/2026_08_11_000004_create_transactions_table.php
+++ database/migrations/2026_08_11_000004_create_transactions_table.php
@@ -7,6 +7,9 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
+        if (Schema::hasTable('transactions')) {
+        return;
+    }
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('vendor_id')->constrained()->cascadeOnDelete();
--- database/migrations/2026_08_17_104523_create_vendors_table.php
+++ database/migrations/2026_08_17_104523_create_vendors_table.php
@@ -8,6 +8,9 @@ return new class extends Migration
 {
     public function up(): void
     {
+         if (Schema::hasTable('vendors')) {
+        return;
+    }
         Schema::create('vendors', function (Blueprint $table) {
             $table->id();
 
